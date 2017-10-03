dcab9ea1f7726f8282d2867cf1f06ab5ba6a8a65
Eliezer Graber <eygraber@gmail.com>
2017-10-03 17:34:13 -0400

    Fix visibility issue with Kotlin (#65)
    * Extract two public static inner classes
    
      - These classes cause an issue with Kotlin
        (https://youtrack.jetbrains.com/issue/KT-11398)

--- scissors/src/main/java/com/lyft/android/scissors/CropView.java
+++ scissors/src/main/java/com/lyft/android/scissors/CropView.java
@@ -38,9 +38,6 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
-import com.lyft.android.scissors.CropViewExtensions.CropRequest;
-import com.lyft.android.scissors.CropViewExtensions.LoadRequest;
-
 import java.io.File;
 import java.io.OutputStream;
 import java.lang.annotation.Retention;
